e1a29f0ba992484b0bef9e730b7e1cf2c74d9f27
attahchisom6 <attahchisom6@gmail.com>
2023-09-21 21:43:03 +0100

    add

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-import ElemMathsController from '../controllers/ElemMathsController';
+import MathsController from '../controllers/MathsController';
 import UIController from '../controllers/UIController';
 
 import express from 'express';
 
 const router = express.Router();
 
-router.get('/basicCalc', ElemMathsController.getBasicCalc);
+router.get('/basicCalc', MathsController.getBasicCalc);
+router.get('/complexCalc', MathsController.getComplexCalc);
 
 // User Interface related issues
 router.get('/', UIController.getUI);
