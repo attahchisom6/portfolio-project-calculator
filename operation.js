7eb1a63c5680a5aebcf4f070b45d60131e718ba8
attahchisom6 <attahchisom6@gmail.com>
2023-09-29 10:16:37 +0100

    add

--- operation.js
+++ operation.js
@@ -1,11 +1,11 @@
-Requirements
+/* Requirements
 A function named handleOperationWithoutOperator that recieves a string argument name expression
   defines a const list of function names precisely const funcs = ['sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'sec', 'cot', 'cosec']
 
   iterates through each element in the list as item
   if item is in the expression
-    check if the previous element prevItem before the item in the expression is an operator or a floatParsible.
-      if prevItem is an operator.
+    check if the previous element prevItem before the item in the expression is an wmpty or a floatParsible.
+      if prevItem is an empty.
         proceed to check if the next elemnt after item nextItem delimited by an operator is within a bracket e.g item80+
         if nextItem is not in bracket, enclose it in a bracket e.g item + '(' + nextItem + ')'.
          else if its in a bracket leave it just like that e.g item(90) will be item(90);
@@ -28,15 +28,48 @@ the essence of this psuedoCode is to implement a function that achieves the foll
 
 4. and genarally if expr = "2 + cos30 + 7cosec(50) + 4tan80" is passed "2 + cos(30) + 7 * cosec(50) + 4 * tan(80)" is retuned
 
-pls if there is something missing in the requirements to achieve these result u can fix or add it and implement a code to thoroughly  satisfy this requirement
+pls if there is something missing in the requirements to achieve these result u can fix or add it and implement a code to thoroughly  satisfy this requirement */
 
 function handleOperationWithoutOperator(expression) {
-  const funcs = ['sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'sec', 'cot', 'cosec'];
+  const funcs = ['sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'sec', 'cot', 'coSec'];
   const splitCriteria = /([-+*/%])/;
+  let refinedExpression = '';
 
   while (expression) {
     for (const item of funcs) {
+      let processed = false;
       if (expression.includes(item)) {
         const itemIndex = expression.indexOf(item);
-        const prevItem = expression.slice(0, index).split(sp)[2].split(item)[0].trim();
-        let nextItem = expression.slice(index + item.length).split(sp)[0]
+        console.log(`index of ${item}: ${itemIndex}`);
+        let prevItem = expression.slice(0, itemIndex).split(splitCriteria);
+        prevItem = prevItem[prevItem.length - 1].trim();
+        console.log('prevvitems:', prevItem);
+        let nextItem = expression.slice(itemIndex + item.length).split(splitCriteria)[0];
+        console.log('next items:', nextItem);
+        if (isFloatParsible(prevItem)) {
+          prevItem = prevItem + '*';
+        }
+        if (!nextItem.match(/\((\w+)\)/)) {
+          nextItem = '(' + nextItem + ')';
+        }
+        refinedExpression = prevItem + item + nextItem;
+        expression = expression.split(itemIndex + item.length);
+        console.log('trubcatedExpression:', expression);
+        processed = true;
+        break;
+      }
+    }
+    if (!processed) {
+      refinedExpression += expression;
+    }
+    break;
+  }
+  console.log('refinedExpression:', refinedExpression);
+  return refinedExpression;
+}
+
+function isFloatParsible(num) {
+  return !isNan(num);
+}
+
+module.exports = handleOperationWithoutOperator; 
