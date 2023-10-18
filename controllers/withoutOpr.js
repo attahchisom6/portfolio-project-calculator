89d496142ee9315c9fcc5883184023565b2634b9
attahchisom6 <attahchisom6@gmail.com>
2023-10-18 20:26:18 +0100

    add

--- controllers/withoutOpr.js
+++ controllers/withoutOpr.js
@@ -1,4 +1,4 @@
-const trigFunctions = ["sin", "cos", "tan",  "cosec", "sec", "cot", "asin", "acos", "atan", "p(anyArg)", 'log', 'In'];
+const trigFunctions = ["sin", "cos", "tan",  "cosec", "sec", "cot", "asin", "acos", "atan", "PI(anyArg)", 'log', 'In'];
 
 function isFloatParsable(num) {
   if (num === '') {
